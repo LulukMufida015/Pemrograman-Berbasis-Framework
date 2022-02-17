b9639fa3b9e2e03b5cd30b1400dd33b9b56b656c
LulukMufida <lulukmufida0015@gmail.com>
2022-02-17 14:52:56 +0700

    Praktikum 1

---
+++ Praktikum 1/arrow.js
@@ -0,0 +1,9 @@
+//Tanpa arrow
+function gretting(message){
+    return alert(`${message} Cantik !`);
+}
+gretting('Selamat Pagi');
+
+//Menggunakan arrow
+// let gretting = message => alert(`${message} Cantik!`);
+// gretting('Selamat Pagi');
