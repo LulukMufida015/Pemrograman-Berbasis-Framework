74d10c1c9ca4bef94c5af494919742e58dfefeb0
LulukMufida <lulukmufida0015@gmail.com>
2022-05-11 21:55:19 +0700

    Firebase-Modul10

---
+++ Praktikum 10/firebase-modul10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDN9Gfdbb2G_avO3nm_eD_3uyYg8MZ6b_0",
+    authDomain: "fir-luluk.firebaseapp.com",
+    projectId: "fir-luluk",
+    storageBucket: "fir-luluk.appspot.com",
+    messagingSenderId: "786557582290",
+    appId: "1:786557582290:web:638bfead2a6370e23ba60b",
+    measurementId: "G-KHG3YRES14"
+};
