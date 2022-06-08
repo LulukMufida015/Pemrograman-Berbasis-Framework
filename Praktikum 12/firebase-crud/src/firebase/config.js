d48853032803db351799225840089b7924c32e80
LulukMufida <lulukmufida0015@gmail.com>
2022-06-08 08:47:48 +0700

    react-laravel

--- Praktikum 12/firebase-crud/src/firebase/config.js
+++ Praktikum 12/firebase-crud/src/firebase/config.js
@@ -1,11 +1,13 @@
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCxCyrZFKWEz0sJZAqa-BqkmCOgXp9ud4I",
-    authDomain: "fir-login-b2558.firebaseapp.com",
-    projectId: "fir-login-b2558",
-    storageBucket: "fir-login-b2558.appspot.com",
-    messagingSenderId: "751430722359",
-    appId: "1:751430722359:web:c413c250592ed31cae30b2",
-    measurementId: "G-X12DW725W4"
-  };
+  apiKey: "AIzaSyCxCyrZFKWEz0sJZAqa-BqkmCOgXp9ud4I",
+  authDomain: "fir-login-b2558.firebaseapp.com",
+  databaseURL: "https://fir-login-b2558-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fir-login-b2558",
+  storageBucket: "fir-login-b2558.appspot.com",
+  messagingSenderId: "751430722359",
+  appId: "1:751430722359:web:c413c250592ed31cae30b2",
+  measurementId: "G-X12DW725W4"
+};
 
 export default firebaseConfig;
